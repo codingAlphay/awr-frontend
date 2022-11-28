c864a76e21d164d7a2a3171de4a39d051fc36f06
Alpay Aksu <2110653914@stud.fh-kufstein.ac.at>
2022-11-28 18:45:25 +0100

    Update Navbar

--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -1,7 +1,10 @@
 export default function Navbar() {
     return (
         <div className="h-24 w-full bg-green">
-
+            <ul>
+                <li>REPORT-GENERATOR</li>
+                <li>REPORT-ARCHIV</li>
+            </ul>
         </div>
     )
 }
