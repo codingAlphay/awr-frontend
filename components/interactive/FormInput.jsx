f4d832b38aad61a7e939f153493b7a6ec34497a0
aksualpay <2110653914@stud.fh-kufstein.ac.at>
2022-12-07 12:18:29 +0100

    Update forminput component

--- components/interactive/FormInput.jsx
+++ components/interactive/FormInput.jsx
@@ -1,5 +1,9 @@
-export default function FormInput() {
+export default function FormInput({value}) {
     return (
-        <></>
+        <div className="relative z-0 w-full mb-6 group">
+            <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-darkblue bg-transparent border-0 border-b-2 border-green appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label htmlFor="floating_email" className="peer-focus:font-medium absolute font-bold text-sm text-darkblue duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-darkblue
+             peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{value}</label>
+        </div>
     )
 }
