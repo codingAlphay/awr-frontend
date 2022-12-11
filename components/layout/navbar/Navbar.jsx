cc437562841d9ec383449f2ef03ebb738b3ca085
aksualpay <2110653914@stud.fh-kufstein.ac.at>
2022-12-11 19:18:42 +0100

    Update Navbar

--- components/layout/navbar/Navbar.jsx
+++ components/layout/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
                     <NavbarElement type={'archive'}/>
                 </div>
             </div>
-            <div className="h-24"/>
+            <div className="h-28"/>
         </>
     )
 }
