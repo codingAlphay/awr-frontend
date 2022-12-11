fd9b4c66be55008d69c77905de8f2d159ed2edf9
aksualpay <2110653914@stud.fh-kufstein.ac.at>
2022-12-11 19:16:53 +0100

    Update title component

--- components/elements/Title.jsx
+++ components/elements/Title.jsx
@@ -2,13 +2,13 @@ export default function Title({value, option}) {
     {/*Option for biggest headline*/}
     if(option == 1) {
         return (
-            <h1 className="text-3xl md:text-4xl font-bold text-darkblue">{value}</h1>
+            <h1 className="text-4xl font-extrabold md:text-5xl text-darkblue">{value}</h1>
         )
     }
     {/*Option for medium headline*/}
     if(option == 2) {
         return (
-            <h2 className="text-2xl md:text-3xl font-bold text-darkblue">{value}</h2>
+            <h2 className="text-2xl font-bold md:text-3xl text-darkblue">{value}</h2>
         )
     }
     {/*Option for report generation progress headline*/}
