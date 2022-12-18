a5a79471f707047caf53d14092f5c84088fda1b7
Alpay Aksu <2110653914@stud.fh-kufstein.ac.at>
2022-12-18 05:15:18 +0100

    Add form empty validation

--- components/layout/generator/ReportGeneratorModal.jsx
+++ components/layout/generator/ReportGeneratorModal.jsx
@@ -3,6 +3,7 @@ import StepTwo from './steps/StepTwo'
 import StepThree from './steps/StepThree'
 import Title from '../../elements/Title'
 import { useState } from 'react'
+import Router from 'next/router'
 import CustomButton from '../../interactive/CustomButton'
 
 const uuid = require('uuid')
@@ -12,7 +13,8 @@ export default function ReportGeneratorModal() {
     const now = new Date().toISOString()
     const reportId = uuid.v4()
     const [viewStep, setViewStep] = useState(0)
-    
+    const [filled, setFilledState] = useState(false)
+
     //ein feld kommt noch hinzu
     const [formData, setFormData] = useState({
         id: reportId,
@@ -50,11 +52,17 @@ export default function ReportGeneratorModal() {
 
     return (
         <>
-        {console.log(viewStep)}
             <div className='w-full max-w-5xl px-16 mx-auto py-11 bg-lightgrey rounded-3xl'>
                 <Title value={'REPORT GENERATOR'} option={1}/>
                 {view()}
             </div>
+            {filled == true && 
+                <div className='flex justify-center'>
+                    <div className='error-message mt-6 border-green py-1 px-4 inline-flex items-center border-4 rounded-md'>
+                        <span className='text-4xl text-green font-extrabold mr-3'>!</span>Bitte überprüfe ob deine Angaben vollständig sind
+                    </div>
+                </div>
+            }
             <div className='flex justify-center items-center mt-4'>
                 {(viewStep == 0) ? (
                     <div className='opacity-70'>
@@ -67,10 +75,32 @@ export default function ReportGeneratorModal() {
                 )}
                 {(viewStep == 2) ? (
                     <button submit onClick={() => {
+                        if(
+                        (formData.driver_first_name && 
+                        formData.driver_last_name && 
+                        formData.driver_city &&
+                        formData.driver_street &&
+                        formData.driver_zip &&
+                        formData.car_license &&
+                        formData.car_manufacturer &&
+                        formData.car_model &&
+                        formData.ev_cleaning &&
+                        formData.pf_change &&
+                        formData.pf_cleaning &&
+                        formData.result_after &&
+                        formData.result_before &&
+                        formData.recommendation &&
+                        formData.month &&
+                        formData.year) != ''
+                        ) {
+                        Router.reload(window.location.pathname);
                         fetch('/api/reporthandler/createreport', {
-                        method: 'post',
-                        body: JSON.stringify(formData)
-                        })
+                            method: 'post',
+                            body: JSON.stringify(formData)
+                            })
+                        } else {
+                            setFilledState(true)
+                        }
                     }}>
                         <CustomButton setting={'text'} value={'Erstellen'} />
                     </button>
